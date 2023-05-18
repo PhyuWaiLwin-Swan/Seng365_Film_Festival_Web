2f53db2704096c0f4c40725d2753f67f1f46e263
pwl24 <pwl24@uclive.ac.nz>
2023-05-19 00:26:11 +1200

    image and create the film

--- src/components/CreateFilm.tsx
+++ src/components/CreateFilm.tsx
@@ -18,15 +18,19 @@ import axios from "axios";
 import domain from "../domain";
 import {useNavigate} from "react-router-dom";
 import MenuItem from "@mui/material/MenuItem";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import Tooltip from '@mui/material/Tooltip';
 import Stack from '@mui/material/Stack';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { Tooltip } from '@mui/material'; // Update the import for Tooltip
+import { Toolbar } from '@mui/material'; // Update the import for Toolbar
+import { Typography as MuiTypography } from '@mui/material'; // Rename the import
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-
+import dayjs, { Dayjs } from 'dayjs';
+import { DemoContainer,DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import defaultImage from './../default-image.png';
+import './../App.css';
 // Inside your component
 
 const CreateFilm = () => {
@@ -49,14 +53,28 @@ const CreateFilm = () => {
     const [errorFlag, setErrorFlag] = React.useState(false)
     const [errorMessage, setErrorMessage] = React.useState("")
     const navigate = useNavigate();
+    const [value, setValue] = React.useState<Dayjs >();
+    const [imageFile, setImageFile] = useState<File | null>(null);
+
+    const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0]; // Use optional chaining
+        if (file) {
+            setImageFile(file);
+        }
+    };
+    const handleRemoveImage =  () => {
+        setImageFile(null);
+
+
+    }
     const submitCreateFilm = (e: { preventDefault: () => void; }) => {
-        setFilm((prevFilm) => ({ ...prevFilm, directorId: localStorage.getItem("userId") as unknown as number}));
-
-
-
             e.preventDefault();
-
-            axios.post(domain  + "/films", {
+        console.log(film);
+            axios.post(domain  + "/films",{
+                headers: {
+            'X-Authorization': localStorage.getItem("token"),
+                'Content-Type': 'application/json'
+        }, data:{
                 "title": film.title,
                 "description": film.description,
                 "releaseDate": film.releaseDate,
@@ -64,18 +82,41 @@ const CreateFilm = () => {
                 "runtime": film.runtime,
                 "ageRating": film.ageRating
                 }
-
+    }
             )
                 .then((response) => {
                     setErrorFlag(false)
                     setErrorMessage("")
-                    navigate('/films/'+response.data.filmId)
+
+                    // navigate('/films/'+response.data.filmId)
+                    setFilm((prevFilm) => ({ ...prevFilm, filmId:  response.data.filmId}))
 
                 }, (error) => {
 
                     setErrorFlag(true)
                     setErrorMessage(error.toString())
                 })
+        console.log(localStorage.getItem("token"));
+
+        // if (imageFile) {
+        //     axios.put(domain + "/films/" + film.filmId, {
+        //             headers: {
+        //                 'X-Authorization': localStorage.getItem("token"),
+        //                 'Content-Type': 'image/gif'
+        //             }, data: imageFile
+        //         }
+        //     )
+        //         .then((response) => {
+        //             setErrorFlag(false)
+        //             setErrorMessage("")
+        //             navigate('/films/' + response.data.filmId)
+        //
+        //         }, (error) => {
+        //
+        //             setErrorFlag(true)
+        //             setErrorMessage(error.toString())
+        //         })
+        // }
     }
     const [genre,setGenre] = useState<Array<Genre>>([]);
     const genreList = genre.map(obj=>obj.name);
@@ -109,169 +150,145 @@ const CreateFilm = () => {
 
 
 
-
-    function Label({
-                       componentName,
-                       valueType,
-                       isProOnly,
-                   }: {
-        componentName: string;
-        valueType: string;
-        isProOnly?: boolean;
-    }) {
-        const content = (
-            <span>
-      <strong>{componentName}</strong> for {valueType} editing
-    </span>
-        );
-
-        if (isProOnly) {
-            return (
-                <Stack direction="row" spacing={0.5} component="span">
-                    <Tooltip title="Included on Pro package">
-                        <a href="/x/introduction/licensing/#pro-plan">
-                            <span className="plan-pro" />
-                        </a>
-                    </Tooltip>
-                    {content}
-                </Stack>
-            );
-        }
-
-        return content;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     const CreateFilmPage = () => {
-        return (<div>
-            <Container style={{display: 'inline-grid', padding: "10px",verticalAlign: 'middle'}} >
+        return <div>
+
+
+            <Container
+                style={{
+                    display: 'inline-block',
+                    width: '600px',
+                    backgroundColor: 'white',
+                    border: '1px solid gray',
+                    borderRadius: '4px',
+                    padding: '60px',
+                    verticalAlign: 'middle'
+
+                }}
+            >
                 <form
-                    // className={classes.root}
                     onSubmit={submitCreateFilm}>
-                <CardMedia style={{padding:'20px'}}
-                           src={domain+`/films/${film.filmId}/image` }
-                           component="img"
-                           height="400"
-                           width="400"
-                           image= {film.image_filename}
-                           alt=""
-                />
-                {/*<> Description : {film.description}</>*/}
+                <h1>Create Film</h1>
+                <div className="eachBox">
 
-                <TextField style={{padding:'10px'}}
-                           id={film.filmId+"_title"}
-                           label="Title"
-                           defaultValue={film.title}
-                           InputProps={{
-                               readOnly: false,
-                           }}
-                           variant="standard"
-                           onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, title:  e.target.value}))}
+                    <Card>
+                        <CardMedia
+                            className="app-img"
+                            component="img"
+                            image={imageFile ? URL.createObjectURL(imageFile) : defaultImage}
+                            alt={imageFile ? 'Uploaded' : 'Default'}
+                        />
+                    </Card>
 
-                />
-                <TextField style={{padding:'8px'}}
-                           id={film.filmId+"_description"}
-                           label="Description"
-                           // defaultValue={film.description}
-                           InputProps={{
-                               readOnly: false,
-                           }}
-                           variant="standard"
-                           multiline
-                           rows={3}
+                </div>
 
-                           onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, description:  e.target.value}))}
-                />
-                <TextField style={{padding:'8px'}}
-                           id={film.filmId+"_runtime"}
-                           label="Run Time"
-                           defaultValue={film.runtime}
-                           InputProps={{
-                               readOnly: false,
-                           }}
-                           variant="standard"
-                           onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, runtime:  parseInt(e.target.value)}))}
-                />
-                    {/*<LocalizationProvider dateAdapter={AdapterDateFns}>*/}
-                    {/*    /!* Your component content *!/*/}
-                    {/*</LocalizationProvider>*/}
+                <div style={{padding: "10px"}}>
+                        <input
+                            type="file"
+                            accept="image/*"
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                handleImageUpload(event)
+                            }
+                        />
 
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DemoContainer
-                            components={[
-                                'DatePicker',
-                                'TimePicker',
-                                'DateTimePicker',
-                                'DateRangePicker',
-                            ]}
+                    {imageFile &&
+                        <Button onClick={handleRemoveImage}>Remove Image</Button>}
+
+
+                    </div >
+                <div className="eachBox">
+                    <TextField style={{width:"100%"}}
+                                   id={film.filmId+"_title"}
+                                   label="Title"
+                                   defaultValue={film.title}
+                                   InputProps={{
+                                       readOnly: false,
+                                   }}
+                                   variant="standard"
+                                   onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, title:  e.target.value}))}
+
+                        />
+                    </div>
+                <div className="eachBox">
+                    <TextField style={{ width:"100%"}}
+                                   id={film.filmId+"_description"}
+                                   label="Description"
+                                   // defaultValue={film.description}
+                                   InputProps={{
+                                       readOnly: false,
+                                   }}
+                                   variant="standard"
+                                   onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, description:  e.target.value}))}
+                        />
+                    </div>
+                    <div className="eachBox">
+                        <TextField style={{ width:"100%"}}
+                                   id={film.filmId+"_runtime"}
+                                   label="Run Time"
+                                   // defaultValue={film.runtime}
+                                   InputProps={{
+                                       readOnly: false,
+                                   }}
+                                   variant="standard"
+                                   onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, runtime:  parseInt(e.target.value)}))}
+                        />
+                    </div>
+                <div className="eachBox">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateTimePicker
+                        className="dateTimeSelecter"
+                        onChange={(date: Dayjs | null | undefined) => {
+                            if (date) {
+                                const formattedDate = dayjs(date).format('YYYY-MM-DD HH:mm:ss');
+                                console.log(formattedDate)
+                                setFilm((prevFilm) => ({ ...prevFilm, releaseDate: formattedDate }));
+                            }
+                        }}
+                        views={['year', 'month', 'day', 'hours', 'minutes', 'seconds']}
+                    />
+
+                </LocalizationProvider>
+                </div>
+
+                    <div className="eachBox"><FormControl fullWidth>
+                        <InputLabel id="select_genre">Genre</InputLabel>
+
+                        <Select
+                            labelId="genre_label"
+                            id="genre"
+                            label="Genre"
+                            onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, genreId:  parseInt(e.target.value as string)}))}
+
                         >
-                            <DemoItem
-                                label={<Label componentName="DateTimePicker" valueType="date time" />}
-                            >
-                                <DateTimePicker />
-                            </DemoItem>
-                        </DemoContainer>
-                    </LocalizationProvider>
-                {/*<TextField style={{padding:'8px'}}*/}
-                {/*           id={film.filmId+"_releaseDate"}*/}
-                {/*           label="Release Date"*/}
-                {/*           defaultValue={film.releaseDate}*/}
-                {/*           InputProps={{*/}
-                {/*               readOnly: false,*/}
-                {/*           }}*/}
-                {/*           variant="standard"*/}
-                {/*           onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, title:  e.target.value}))}*/}
-                {/*/>*/}
-                <FormControl fullWidth>
-                    <InputLabel id="select_genre">Genre</InputLabel>
-
-                    <Select
-                        labelId="genre_label"
-                        id="genre"
-                        label="Genre"
-                        onChange= {(e) => setFilm((prevFilm) => ({ ...prevFilm, genreId:  parseInt(e.target.value as string)}))}
-
-                    >
-                        {genre.map(({ genreId, name }) => (
-                            <MenuItem key={genreId} value={genreId}>
+                            {genre.map(({ genreId, name }) => <MenuItem key={genreId} value={genreId}>
                                 {name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <FormControl fullWidth>
-                    <InputLabel id="select_ageRating">Age Rating</InputLabel>
+                            </MenuItem>)}
+                        </Select>
+                    </FormControl>
+                    </div>
 
-                    <Select
-                        labelId="ageRating_label"
-                        id="ageRating"
-                        label="ageRating"
-                        onChange={(e) => setFilm((prevFilm) => ({ ...prevFilm, ageRating:  e.target.value as string}))}
-                    >
-                        {ageRatingList.map((age) => (
-                            <MenuItem key={age} value={age} >
-                                {age}
-                            </MenuItem>
-                        ))}
+                <div className="eachBox">
+                    <FormControl fullWidth>
+                        <InputLabel id="select_ageRating">Age Rating</InputLabel>
 
-                    </Select>
-                </FormControl>
+                        <Select
+                            labelId="ageRating_label"
+                            id="ageRating"
+                            label="ageRating"
+                            onChange={(e) => setFilm((prevFilm) => ({ ...prevFilm, ageRating:  e.target.value as string}))}
+                        >
+                            {ageRatingList.map((age) => <MenuItem key={age} value={age} >
+                                    {age}
+                                </MenuItem>)}
+
+                        </Select>
+                    </FormControl>
+                </div>
+                    <Button style={{height: "55px"}} type="submit" variant="contained">Submit</Button>
                 </form>
 
             </Container>
-        </div>)
+        </div>
 
 
     }
