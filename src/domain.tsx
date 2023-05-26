d99966a53a73e9107101dfe399e993f2a0042d98
pwl24 <pwl24@uclive.ac.nz>
2023-05-26 15:32:32 +1200

    change to local host

--- src/domain.tsx
+++ src/domain.tsx
@@ -1,6 +1,6 @@
 
 
-const domain = "https://seng365.csse.canterbury.ac.nz/api/v1"
-// const domain = "http://localhost:8080/api/v1"
+// const domain = "https://seng365.csse.canterbury.ac.nz/api/v1"
+const domain = "http://localhost:8080/api/v1"
 
 export default domain;
